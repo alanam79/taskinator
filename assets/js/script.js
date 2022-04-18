469386f92a4a11d6e775e61f4d76f2aaa9d8f494
Jim Gryck <halo2382001@yahoo.com>
2022-04-18 16:11:29 -0400

    updated code to include console.dir

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,6 +38,8 @@ var createTaskEl = function(taskDataObj) {
   taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
   listItemEl.appendChild(taskInfoEl);
 
+  console.dir(listItemEl);
+
   // add entire list item to list
   tasksToDoEl.appendChild(listItemEl);
 }
